ad9059d968060ed5326d826adca5746a8600f6f3
tracer-nju <tracer@nju.edu.cn>
2020-10-06 23:02:58 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: 73a467a61f9c4502a5aeab414e55527a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  23:02:58 up 35 min,  1 user,  load average: 2.93, 2.24, 1.81

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,26 @@
 #include <stdint.h>
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+
+  uint64_t ans;
+  
+  a%=m;
+
+  while(b)
+  {
+    if(b&1)//如果b是奇数
+    {
+      ans=(ans+a)%m;
+
+    }
+
+
+    a=(2*a)%m;
+
+    b>>=1;//b/=2;
+  }
+
+  return ans;
+
+
 }
