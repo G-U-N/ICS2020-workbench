865be3e867c893c9ae03f06128372b39a5545c01
tracer-nju <tracer@nju.edu.cn>
2020-10-07 10:12:20 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: 73a467a61f9c4502a5aeab414e55527a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  10:12:20 up 11:45,  1 user,  load average: 1.33, 1.38, 1.20

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
 
+#define half 9223372036854775808
+#define whole 18446744073709551616
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
  
   uint64_t ans=0;
@@ -10,13 +13,22 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   {
     if(b&1)//如果b是奇数
     {
+      //这一步的ans+a可能已经出现了溢出
       ans=(ans+a)%m;
 
     }
 
+     //这一步的a+a可能已经出现了溢出。 
 
-    a=(a+a)%m;
-
+    if (a<half) 
+    {
+      a=(a+a)%m;
+    }
+    else
+    {
+      a=((a-half)+(a-half))%m+(whole-1)%m+1;
+    }
+    
     b>>=1;//b/=2;
   }
 
