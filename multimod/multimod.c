97b2c9a021e1ef153d2b0fac5f136ee83dabfe81
tracer-nju <tracer@nju.edu.cn>
2020-10-10 18:24:25 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: 9720ce3c446b44e2a13275eb9fcf56f0   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  18:24:25 up 11 min,  1 user,  load average: 0.67, 0.68, 0.40

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -2,82 +2,19 @@
 #include <stdio.h>
 
 
-
-const uint64_t half=9223372036854775808UL;
-#define whole 18446744073709551616;
-//如果能够处理好a+b取模m，我就能通过所有样例。
-
-
-uint64_t mod(uint64_t a, uint64_t m)
-{
-  //当m是2的整数次幂时。
-  if((!(m&(m-1)) )&& m)
-  {
-    int k;
-    for( k=0;(k<64)&& !((1<<k)&m);k++);
-
-    return a&((1<<k)-1);
-  }
-  while(a>=m)
-  {
-    a-=m;
-  }
-  return a;
-  
-}
-
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
- 
-  uint64_t ans=0;
-  
-  a=mod(a,m);
-
-  while(b)
-  {
-    if(b&1)//如果b是奇数
-    {
-
-      ans=mod((ans+a),m);
-
-    }
-
-    a=mod(a<<1,m);
-    b>>=1;//b/=2;
-  }
-
-  return ans;
-
-
-}
-
-
-//变相的快速幂。
-/*
-uint64_t mod(uint64_t v, uint64_t m)
-{
-    //当m是2的整数次幂时。用于秒过简单样例
-    while(v>=m)
-    {
-      v-=m;
-    }
-    return v;
-    
-}
-uint64_t multimod(uint64_t a, uint64_t b, uint64_t m)
+uint64_t multimod(uint64_t a,uint64_t b,uint64_t m)
 {
   uint64_t ans=0;
-  for(int i=0;i<64;i++)
+
+  while(b>0)
   {
-    int temp=(a>>i)&1UL;
-    if(temp)
+    if (b&1)
     {
-      ans+=mod((b<<i),m);
+      ans=(ans+a)%m;
     }
-    ans=mod(ans,m);
+    a=(a+a)%m;
+
+    b>>=1;
   }
-  return ans;
-
-
-
+  
 }
-*/
