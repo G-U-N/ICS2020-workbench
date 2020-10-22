f392c08ec13175bf9ae974c4a4fe903785f49e4a
tracer-nju <tracer@nju.edu.cn>
2020-10-22 10:49:18 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: f8efb8a531da44c9bfd64fcf01051a48   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  10:49:18 up  3:34,  1 user,  load average: 0.73, 1.05, 1.15

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -22,7 +22,7 @@ int asm_popcnt(uint64_t x) {
   asm(
   "movl $0, %%eax;"//ans
   "movl $0, %%ecx;"//i
-  "cycle:cmpl $64, %%ecx;"
+  "cycle:cmpl $0x40, %%ecx;"
   "jge label;"
   "movq %%rbx, %%rdx;"
   "shrq %%ecx, %%rdx;"
