d315516aa50b2e351557d8775746146688507cbc
tracer-nju <tracer@nju.edu.cn>
2020-10-22 11:03:04 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: f8efb8a531da44c9bfd64fcf01051a48   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  11:03:04 up  3:48,  1 user,  load average: 0.95, 0.81, 0.95

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -25,21 +25,21 @@ int asm_popcnt(uint64_t x) {
   "cycle:cmpl $64, %%ecx;"
   "jge label;"
   "movq %%rbx, %%rdx;"
-  "and $1,%%rdx;"
-  "addl %%edx,%%eax;"
+  "shrq %%ecx, %%rdx;"
+  "and $1, %%rdx;"
+  "cmpq $1, %%rdx;"
+  "je t1;"
+  "jmp t2;"
+  "t1:"
+  "incl %%eax;"
+  "t2:"
   "incl %%ecx;"
-  "shrq $1,%%rbx;"
   "jmp cycle;"
   "label:"
   :"=a"(ans)
   :"b"(x));
   return ans;
 }
-//The shift is repeated the number of times indicated by the second operand, 
-//which is either an immediate number or the contents of the CL register.
-// To reduce the maximum execution time, the 80386 does not allow shift counts greater than 31.
-// If a shift count greater than 31 is attempted, only the bottom five bits of the shift count are used. 
-//(The 8086 uses all eight bits of the shift count.)
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
   return memcpy(dest, src, n);
