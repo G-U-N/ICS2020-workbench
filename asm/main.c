0518d2195d0e9e3c6cbf53dc685c8339931cf43a
tracer-nju <tracer@nju.edu.cn>
2020-10-23 10:27:09 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: f8efb8a531da44c9bfd64fcf01051a48   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  10:27:09 up 1 day,  3:12,  1 user,  load average: 0.62, 0.49, 0.43

--- asm/main.c
+++ asm/main.c
@@ -8,13 +8,16 @@ int main() {
   printf("%ld\n",asm_add(1234,5678));
 
   printf("%d\n",asm_popcnt(0x0123456789abcdefULL));
-
+  
   const char src[50]="hello world!";
   char dest[50];
   asm_memcpy(dest,src,strlen(src)+1);
   printf("dest=%s\n",dest);
 
-   asm_jmp_buf buf;
+  asm_jmp_buf buf;
+
+  printf("%d\n",asm_setjmp(buf));
+  
   int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
