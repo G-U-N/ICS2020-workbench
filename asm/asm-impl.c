786b735e9c313eca0bbad08a74c39ecdaab98ee6
tracer-nju <tracer@nju.edu.cn>
2020-11-03 17:51:21 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: 7319470ae21f491282a84afd8b74a1ec   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  17:51:21 up  7:05,  1 user,  load average: 1.18, 0.87, 0.96

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -77,7 +77,7 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
   asm("movq $0 %%rsi;"
   "cycle_memcpy: cmpq %%rsi, %%rcx;"
   "jbe end_memcpy;"
-  "mov (%%rbx,%%rsi,1),%%rdx;"
+  "movzbl (%%rbx,%%rsi,1),%%dl;"
   "mov %%dl, (%%rax,%%rsi,1);"
   "inc %%rsi;"
   "jmp cycle_memcpy;"
