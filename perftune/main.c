38725830effcf4da5c5f5d6de36ebc5900de5265
tracer-nju <tracer@nju.edu.cn>
2020-11-25 15:08:33 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: 14dc19fe2bb84decb5eb60b5e25dd325   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  15:08:33 up 43 min,  1 user,  load average: 1.06, 0.95, 0.71

--- perftune/main.c
+++ perftune/main.c
@@ -4,9 +4,9 @@
 int *sieve(int n);
 
 int main() {
-  int *primes = sieve(400);
+  int *primes = sieve(1000000);
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
-  }
+
 }
