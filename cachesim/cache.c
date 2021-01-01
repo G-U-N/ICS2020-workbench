b533d1db80466f8c8dda48cbaa5e1ed7f6b10805
tracer-nju <tracer@nju.edu.cn>
2021-01-02 07:07:31 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: f638fc85b57b469bbbc899d0541b3f98   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  07:07:31 up  1:45,  1 user,  load average: 1.66, 0.87, 0.75

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -22,7 +22,7 @@ uint32_t cache_read(uintptr_t addr) {
   uint32_t group_id   = (addr>>6)&(CACHE_GROUP_NUM-1);
   int tmp=0;
   for (int i =1 ; i < = CACHE_GROUP_NUM ;i>>2) tmp++; 
-  print("%d",tmp);
+  printf("%d",tmp);
   uint32_t tag = (addr>>12);
 
   return 0;
