04b30c85acf6da20368f9d1bd2c47c7fc269e36e
tracer-nju <tracer@nju.edu.cn>
2021-01-02 09:49:29 +0800

    Static hostname: wongfrey-KLV-WX9          Icon name: computer-laptop            Chassis: laptop         Machine ID: ae72fdb29350412d8f0dc81687ae7d12            Boot ID: f638fc85b57b469bbbc899d0541b3f98   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  09:49:29 up  4:27,  1 user,  load average: 1.08, 0.81, 0.75

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -18,7 +18,7 @@ typedef struct
     // bool dirty;
 }Cache;
 
-#define MAX_GROUP_NUM  (int)65537
+#define MAX_GROUP_NUM  (int)65536
 #define MAX_LINE_NUM (int)100
 static int hit_num=0;
 static int miss_num=0;
